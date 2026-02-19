9c283462987c3eb9d5e9cd2b1553507a19b39aca
joseph kibithe <joekibithe2@gmail.com>
2026-02-20 01:52:44 +0300

    feat: Integrate Vercel Speed Insights by adding the package and component to the layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
 import { Inter, Orbitron } from "next/font/google";
 import "./globals.css";
-import ContextProvider from '@/context' // <--- Import the new provider
+import ContextProvider from "@/context"; // <--- Import the new provider
 import { headers } from "next/headers"; // Required for cookie handling
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
-const orbitron = Orbitron({ subsets: ["latin"], variable: '--font-orbitron' });
+const orbitron = Orbitron({ subsets: ["latin"], variable: "--font-orbitron" });
 
 export const metadata: Metadata = {
   title: "AHADI",
@@ -18,14 +19,16 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   // Required for Reown/Wagmi to work with SSR
-  const cookies = (await headers()).get('cookie')
+  const cookies = (await headers()).get("cookie");
 
   return (
     <html lang="en">
-      <body className={`${inter.className} ${orbitron.variable}`} suppressHydrationWarning>
-        <ContextProvider cookies={cookies}>
-            {children}
-        </ContextProvider>
+      <body
+        className={`${inter.className} ${orbitron.variable}`}
+        suppressHydrationWarning
+      >
+        <ContextProvider cookies={cookies}>{children}</ContextProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
